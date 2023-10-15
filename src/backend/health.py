2c926d64d9829695a261886b2f1da12ba523a9ce
Tony <tuk92144@temple.edu>
2023-10-15 14:01:02 -0400

    test for health.py
    getSteps

--- src/backend/health.py
+++ src/backend/health.py
@@ -1,8 +1,12 @@
-#class Health:
+# class Health:
 
 def calculate_bmi(height, weight):
-    bmi = (weight / (height * height)) * 703  
+    bmi = (weight / (height * height)) * 703
     bmi = round(bmi, 1)
     print(bmi)
     return bmi
-    
+
+
+def getsteps(steps):
+    print(steps)
+    return steps
--- tests/test_health.py
+++ tests/test_health.py
@@ -1,5 +1,9 @@
 import pytest
 from src.backend.health import calculate_bmi
+from src.backend.health import getsteps
 
 def test_bmi():
     assert calculate_bmi(70, 165) == 23.7
+
+def test_getsteps():
+    assert getsteps(1000) == 1000
