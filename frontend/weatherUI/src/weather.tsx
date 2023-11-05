0129589edd92dc0233fbe736fbdce0a897c8bc60
Tony <tuk92144@temple.edu>
2023-11-05 16:00:50 -0500

    swtich cases
    added

--- frontend/weatherUI/src/weather.tsx
+++ frontend/weatherUI/src/weather.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import CloudBoltSVG from "./svg/cloud-bolt-svgrepo-com.svg";
+import CloudRainAltSVG from "./svg/cloud-rain-alt-svgrepo-com.svg";
+import CloudSunAltSVG from "./svg/cloud-sun-alt-svgrepo-com.svg";
 import CloudsSVG from "./svg/clouds-svgrepo-com.svg";
-
-
+import SnowAltSVG from "./svg/snow-alt-svgrepo-com.svg";
 
 interface WeatherData {
   temperature: number;
@@ -9,41 +11,89 @@ interface WeatherData {
   uvIndex: number;
 }
 
+type WeatherCondition = 'Thunderstorm' | 'Rain' | 'Clear' | 'Clouds' | 'Snow';
+
+function mapWeatherDescriptionToCondition(description: string): WeatherCondition {
+  switch (description.toLowerCase()) {
+    case 'thunderstorm':
+      return 'Thunderstorm';
+    case 'rain':
+      return 'Rain';
+    case 'clear':
+      return 'Clear';
+    case 'clouds':
+      return 'Clouds';
+    case 'snow':
+      return 'Snow';
+    default:
+      return 'Clouds'; 
+  }
+}
+
+function WeatherIcon({ condition }: { condition: WeatherCondition }) {
+  let svgSrc;
+  
+  switch (condition) {
+    case 'Thunderstorm':
+      svgSrc = CloudBoltSVG;
+      break;
+    case 'Rain':
+      svgSrc = CloudRainAltSVG;
+      break;
+    case 'Clear':
+      svgSrc = CloudSunAltSVG;
+      break;
+    case 'Clouds':
+      svgSrc = CloudsSVG;
+      break;
+    case 'Snow':
+      svgSrc = SnowAltSVG;
+      break;
+    default:
+      svgSrc = CloudsSVG; 
+      break;
+  }
+
+  return <img src={svgSrc} alt={condition} width="300" height="300" />;
+}
+
 const TemperatureDisplay: React.FC = () => {
   const [weatherData, setWeatherData] = useState<WeatherData>({
     temperature: 0,
-    weatherDescription: '',
+    weatherDescription: '', 
     uvIndex: 0,
   });
 
   useEffect(() => {
     async function fetchWeatherData() {
-      const url = "https://api.open-meteo.com/v1/forecast?latitude=39.9523&longitude=-75.1638&current=temperature_2m,precipitation,rain,weathercode&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,uv_index_max,precipitation_probability_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York";
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        setWeatherData({
-          temperature: data.current.temperature_2m,
-          weatherDescription: data.current.weatherDescription,
-          uvIndex: data.daily.uv_index_max[0],
-        });
-      } catch (error) {
-        console.error("There was a problem fetching the weather data:", error);
-      }
+    
+      const apiWeatherDescription = 'clear'; 
+
+      const mappedCondition = mapWeatherDescriptionToCondition(apiWeatherDescription);
+
+     
+      const data = {
+        current: { temperature_2m: 75 },
+        daily: { uv_index_max: [5] }
+      };
+
+      setWeatherData({
+        temperature: data.current.temperature_2m,
+        weatherDescription: mappedCondition,
+        uvIndex: data.daily.uv_index_max[0],
+      });
     }
 
     fetchWeatherData();
-    const intervalId = setInterval(fetchWeatherData, 60000);
+    const intervalId = setInterval(fetchWeatherData, 60000); 
+
+  
     return () => clearInterval(intervalId);
   }, []);
 
   return (
     <div className="temperature-display">
-      {/* SVG directly in your component */}
-      <img src={CloudsSVG} alt="Clouds" width="100" height="100" />
+      <WeatherIcon condition={weatherData.weatherDescription as WeatherCondition} />
       <p>The current temperature is: {weatherData.temperature}°F</p>
       <p>Weather Condition: {weatherData.weatherDescription}</p>
       <p>UV Index: {weatherData.uvIndex}</p>
