c5a4587c18c322346ae2bfa9bb90f78b8d559530
All-Sait <lydecharles@yahoo.com>
2023-11-12 18:53:22 -0500

    Camera Detection and display activation

--- src/backend/Camera.py
+++ src/backend/Camera.py
@@ -7,39 +7,13 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 
 camera = cv2.VideoCapture(0)
 
-'''
-# Define constants
-SC_MONITORPOWER = 0xF170
-MONITOR_OFF = 2
-MONITOR_ON = -1
-
-def control_display_power(mode):
-    SendMessage = ctypes.windll.user32.SendMessageW
-    HWND_BROADCAST = 0xFFFF
-    WM_SYSCOMMAND = 0x0112
-
-    SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, mode)
-
-control_display_power(MONITOR_OFF)
-
-
-
-time.sleep(10)
-
-'''
-
 print("Starting")
-
 os.popen("xset -display :0.0 dpms force off")
 interval = 0
 while True:
     ret, frame = camera.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4, minSize=(30, 30))
-
-    #cv2.imshow('Face Detection', frame)
-
     if len(faces) > 0:
        print("Face detected")
        os.popen("xset -display :0.0 dpms force on")
