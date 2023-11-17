4a0480152a028bd71431e93923bccfcc5fb723dc
All-Sait <lydecharles@yahoo.com>
2023-11-16 23:52:05 -0500

    opt

--- src/backend/SystemStart.sh
+++ src/backend/SystemStart.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export PATH=$PATH:/usr/local/bin   # Adjust the path as needed
-export NODE_ENV=production
-export PATH=/usr/local/bin:/usr/bin:/bin
 echo "Starting Mirror"
 
 echo "Updating git repo"
