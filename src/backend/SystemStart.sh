d00c0143fb952c056356cf3c360d50ef99c487d2
All-Sait <lydecharles@yahoo.com>
2023-11-16 21:38:42 -0500

    Force set SYS PATH in bash script

--- src/backend/SystemStart.sh
+++ src/backend/SystemStart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/bin   # Adjust the path as needed
+export NODE_ENV=production
+export PATH=/usr/local/bin:/usr/bin:/bin
 echo "Starting Mirror"
 
 echo "Updating git repo"
