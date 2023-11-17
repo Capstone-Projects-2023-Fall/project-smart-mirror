b2be71bcd1bf2914cb6b630dd958de3905ef9661
All-Sait <lydecharles@yahoo.com>
2023-11-16 23:36:22 -0500

    concurrently install line

--- src/backend/SystemStart.sh
+++ src/backend/SystemStart.sh
@@ -13,6 +13,7 @@ echo "Installing packages"
 cd /home/smartmirror/Desktop/project-smart-mirror/frontend/mirror-frontend
 npm update
 npm i
+npm install concurrently
 
 echo "Starting mirror"
 npm run dev
