db98eab0df30b57c30a0607d827d6930a6b9a7f5
All-Sait <lydecharles@yahoo.com>
2023-11-16 20:53:55 -0500

    Appended Bash file with Shabang and changed pull repo

--- src/backend/SystemStart.sh
+++ src/backend/SystemStart.sh
@@ -4,7 +4,7 @@ echo "Starting Mirror"
 echo "Updating git repo"
 cd /home/smartmirror/Desktop/project-smart-mirror
 git stash
-git pull origin main
+git pull origin SM-161
 
 echo "Installing packages"
 cd /home/smartmirror/Desktop/project-smart-mirror/frontend/mirror-frontend
