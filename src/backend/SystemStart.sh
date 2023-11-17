323455e4817876328d5d21c736822e6ef20062f5
All-Sait <lydecharles@yahoo.com>
2023-11-16 19:50:20 -0500

    Created bashfile

---
+++ src/backend/SystemStart.sh
@@ -0,0 +1,15 @@
+echo "Starting Mirror"
+
+echo "Updating git repo"
+cd /home/smartmirror/Desktop/project-smart-mirror
+git stash
+git pull origin main
+
+echo "Installing packages"
+cd /home/smartmirror/Desktop/project-smart-mirror/frontend/mirror-frontend
+npm i
+
+echo "Starting mirror"
+npm run dev
+
+echo "System intialized"
