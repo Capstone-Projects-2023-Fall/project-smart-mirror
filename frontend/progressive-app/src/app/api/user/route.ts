ffbb2f51b4980851c289255767eb2f074c0dc363
mars123123 <tul51449@temple.edu>
2023-11-22 13:50:52 -0500

    added post, doesnt work with rls yet

--- frontend/progressive-app/src/app/api/user/route.ts
+++ frontend/progressive-app/src/app/api/user/route.ts
@@ -4,8 +4,8 @@ import { supabase} from "~/lib/supabase-client";
 
 export async function GET(request: Request){
     const {searchParams} = new URL(request.url)
-    const id = searchParams.get('id')
-    const field = searchParams.get('field')
+    const id = searchParams.get("id")
+    const field = searchParams.get("field")
     
     if (id && field){
         
@@ -18,6 +18,35 @@ export async function GET(request: Request){
         }
         else{
             return NextResponse.json({ message: "Error: Data not found" }, {status: 400})
+        }
+        
+        
+    }  
+
+    return NextResponse.json({ message: "Error: Parameters not found" }, {status: 400})
+}
+//(auth.uid() = id)
+export async function POST(request: Request){
+    const {searchParams} = new URL(request.url)
+    const id = searchParams.get("id")
+    const field = searchParams.get("field")
+    const value = searchParams.get("value")
+    
+    if (id && field && value){
+        const updateObject = {
+            [field]: value,
+          };
+
+        const { data, error } = await supabase
+        .from("profiles")
+        .update(updateObject)
+        .eq('id', id)
+        
+        if (data){
+            return NextResponse.json({ message: data }, {status: 200})
+        }
+        else{
+            return NextResponse.json({ message: error }, {status: 400})
         }
         
         
