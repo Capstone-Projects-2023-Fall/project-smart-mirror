00b01d7844aa7a29e74ebaa46cc2c89599988c0c
mars123123 <tul51449@temple.edu>
2023-12-02 15:44:50 -0500

    added news widget with basic UI. added fitbit widget placeholder

--- frontend/mirror-frontend/src/components/App.tsx
+++ frontend/mirror-frontend/src/components/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import TemperatureDisplay from './weather';
 import CalendarUI from './calendar';
+import FitbitDataComponent from './fitbit'
+import NewsComponent from './news';
 import './App.css'; // Ensure this is the path to your CSS file
 
 const UI = () => {
   return (
+    <div className="ui-container2">
     <div className="ui-container">
       <div className="weather-box">
         <TemperatureDisplay />
       </div>
       <div className="calendar-box">
         <CalendarUI />
+      </div>
+      <div className="fitbix-box">
+      <FitbitDataComponent/>
+      </div>
+      
+    </div>
+    <div className="news-box">
+      <NewsComponent/>
       </div>
     </div>
   );
