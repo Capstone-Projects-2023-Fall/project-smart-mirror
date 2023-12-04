98edeb4665f89c7148d66623032529dabc8ed3e9
Tony <tuk92144@temple.edu>
2023-12-04 04:55:34 -0500

    Can read from toggle and set to database

--- frontend/mirror-frontend/src/components/App.tsx
+++ frontend/mirror-frontend/src/components/App.tsx
@@ -8,33 +8,41 @@ import './App.css';
 import './clock.css';
 
 const UI: React.FC = () => {
+  // Assume that the initial state is set to what is saved in the database
   const [widgetVisibility, setWidgetVisibility] = useState({
-    Weather: true,
-    Calendar: true,
-    News: true,
-    Fitbit: true
+    Weather: false,
+    Calendar: false,
+    News: false,
+    Fitbit: false
   });
 
   useEffect(() => {
+    // Define a function that will be called to fetch widget visibility
     const fetchWidgetVisibility = async () => {
       try {
-        const response = await fetch('http://localhost:1023/api/widget-visibility'); 
+        // Fetch the latest widget visibility state from your API
+        const response = await fetch('http://localhost:1023/api/widget-visibility');
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        setWidgetVisibility(prevVisibility => ({
-          ...prevVisibility,
-          ...data
-        }));
+        
+        // Update the state if the fetched data has the 'downloaded_modules' key
+        if (data && data.downloaded_modules) {
+          setWidgetVisibility(data.downloaded_modules);
+        }
       } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
       }
     };
 
+    // Call the fetch function when the component mounts
     fetchWidgetVisibility();
+
+    // Optionally, set up a polling mechanism to fetch the latest visibility
     const intervalId = setInterval(fetchWidgetVisibility, 5000); // Poll every 5 seconds
 
+    // Clean up the interval on component unmount
     return () => clearInterval(intervalId);
   }, []);
 
@@ -51,7 +59,6 @@ const UI: React.FC = () => {
             <CalendarUI />
           </div>
         )}
-        {/* Clock component is always displayed */}
         <div className="clock-box">
           <Clock />
         </div>
@@ -61,9 +68,13 @@ const UI: React.FC = () => {
           <NewsComponent />
         </div>
       )}
-      {/* The Fitbit component is commented out, so it won't be displayed */}
+      {/* If you want to include Fitbit widget as well */}
+      {widgetVisibility.Fitbit && (
+        <div className="fitbit-box">
+          <FitbitDataComponent />
+        </div>
+      )}
     </div>
-    
   );
 };
 
--- frontend/progressive-app/src/app/dashboard/module_view.tsx
+++ frontend/progressive-app/src/app/dashboard/module_view.tsx
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
 import './ToggleSwitch.css';
 
-const ModuleView: React.FC = () => {
+// Initialize Supabase client
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and Anon Key must be defined');
+}
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+const ModuleView = () => {
   const [widgetVisibility, setWidgetVisibility] = useState({
     Weather: true,
     News: true,
@@ -10,46 +21,53 @@ const ModuleView: React.FC = () => {
 
   const userId = '21380693-3ade-4951-82c0-1440aaf54297'; // Replace with the actual user ID from the user's session
 
-  // Function to fetch the current widget visibility state from the server
+  // Function to fetch the current widget visibility state from Supabase
   const fetchWidgetVisibility = async () => {
-    try {
-      const response = await fetch('http://localhost:1023/api/widget-visibility');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      setWidgetVisibility(data);
-    } catch (error) {
-      console.error('There was an error fetching the widget visibility:', error);
+    const { data, error } = await supabase
+      .from('profiles') // Replace with your actual table name
+      .select('downloaded_modules')
+      .eq('id', userId) // Replace 'id' with the actual column name for user identification
+      .single();
+
+    if (error) {
+      console.error('Error fetching widget visibility:', error);
+    } else if (data) {
+      setWidgetVisibility(data.downloaded_modules);
     }
   };
-
-  useEffect(() => {
-    fetchWidgetVisibility();
-  }, []);
 
   // Function to handle the visibility toggle for each widget
   const toggleWidgetVisibility = async (widgetName) => {
     const newState = !widgetVisibility[widgetName];
 
-    try {
-      const response = await fetch('http://localhost:1023/api/toggle-widget', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ widgetName, newVisibility: newState }),
-      });
+    // Prepare the updated visibility state
+    const updatedVisibility = { ...widgetVisibility, [widgetName]: newState };
 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+    // Update the state locally for a responsive UI
+    setWidgetVisibility(updatedVisibility);
+
+    try {
+      const { error } = await supabase
+        .from('profiles') // Replace with your actual table name
+        .update({ downloaded_modules: updatedVisibility })
+        .eq('id', userId); // Replace 'id' with the actual column name for user identification
+
+      if (error) {
+        throw new Error(error.message);
       }
-      const data = await response.json();
-      setWidgetVisibility(prev => ({ ...prev, [widgetName]: data[widgetName] }));
+      // Log success and keep the updated local state as is
+      console.log(`Visibility for ${widgetName} updated to: ${newState}`);
     } catch (error) {
-      console.error('Error toggling widget visibility:', error);
+      console.error(`Failed to update visibility for ${widgetName}: ${error}`);
+      // Revert the state change for the specific widget if the update fails
+      setWidgetVisibility(prevState => ({ ...prevState, [widgetName]: !newState }));
     }
   };
+
+  // Effect to fetch widget visibility on component mount
+  useEffect(() => {
+    fetchWidgetVisibility();
+  }, []);
 
   return (
     <div className="module-view">
@@ -69,5 +87,6 @@ const ModuleView: React.FC = () => {
       ))}
     </div>
   );
-      }
+};
+
 export default ModuleView;
