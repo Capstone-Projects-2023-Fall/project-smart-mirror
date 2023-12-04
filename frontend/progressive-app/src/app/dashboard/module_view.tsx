1e89fc36a4be7bedfdbab2d9d38b838796e12a88
devinrankin <devinrankin@temple.edu>
2023-12-04 13:04:56 -0500

    fixed deployment issue in module_view.tsx

--- frontend/progressive-app/src/app/dashboard/module_view.tsx
+++ frontend/progressive-app/src/app/dashboard/module_view.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './toggleswitch.css';
+import React, { useState, useEffect } from "react";
+import "./toggleswitch.css";
 
 interface WidgetVisibility {
   Weather: boolean;
@@ -7,27 +7,31 @@ interface WidgetVisibility {
   Calendar: boolean;
 }
 
-
 const ModuleView: React.FC = () => {
   const [widgetVisibility, setWidgetVisibility] = useState<WidgetVisibility>({
     Weather: true,
     News: true,
-    Calendar: true
+    Calendar: true,
   });
 
-  const userId = '21380693-3ade-4951-82c0-1440aaf54297'; // Replace with the actual user ID from the user's session
+  const userId = "21380693-3ade-4951-82c0-1440aaf54297"; // Replace with the actual user ID from the user's session
 
   // Function to fetch the current widget visibility state from the server
   const fetchWidgetVisibility = async () => {
     try {
-      const response = await fetch('http://localhost:1023/api/widget-visibility');
+      const response = await fetch(
+        "http://localhost:1023/api/widget-visibility",
+      );
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       const data = await response.json();
       setWidgetVisibility(data);
     } catch (error) {
-      console.error('There was an error fetching the widget visibility:', error);
+      console.error(
+        "There was an error fetching the widget visibility:",
+        error,
+      );
     }
   };
 
@@ -36,38 +40,43 @@ const ModuleView: React.FC = () => {
   }, []);
 
   // Function to handle the visibility toggle for each widget
-  const toggleWidgetVisibility = async (widgetName : keyof WidgetVisibility) => {
+  const toggleWidgetVisibility = async (widgetName: keyof WidgetVisibility) => {
     const newState = !widgetVisibility[widgetName];
 
     try {
-      const response = await fetch('http://localhost:1023/api/toggle-widget', {
-        method: 'POST',
+      const response = await fetch("http://localhost:1023/api/toggle-widget", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ widgetName, newVisibility: newState }),
       });
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       const data = await response.json();
-      setWidgetVisibility(prev => ({ ...prev, [widgetName]: data[widgetName] }));
+      setWidgetVisibility((prev) => ({
+        ...prev,
+        [widgetName]: data[widgetName],
+      }));
     } catch (error) {
-      console.error('Error toggling widget visibility:', error);
+      console.error("Error toggling widget visibility:", error);
     }
   };
 
   return (
     <div className="module-view">
-      {Object.keys(widgetVisibility).map(widgetName => (
+      {Object.keys(widgetVisibility).map((widgetName) => (
         <div key={widgetName} className="toggle-switch-container">
           <span className="widget-name">{widgetName}</span>
           <label className="toggle-switch">
             <input
               type="checkbox"
-              checked={widgetVisibility[widgetName]}
-              onChange={() => toggleWidgetVisibility(widgetName)}
+              checked={widgetVisibility[widgetName as keyof WidgetVisibility]}
+              onChange={() =>
+                toggleWidgetVisibility(widgetName as keyof WidgetVisibility)
+              }
               className="toggle-switch-checkbox"
             />
             <span className="toggle-switch-slider" />
@@ -76,5 +85,5 @@ const ModuleView: React.FC = () => {
       ))}
     </div>
   );
-      }
+};
 export default ModuleView;
