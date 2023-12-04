7eec37fe3a7d5a509dcdb5c152f9da82968aeb27
tonytran40 <tonytran40@gmail.com>
2023-12-03 22:58:59 -0500

    clock moved to mirror-frontend

--- frontend/mirror-frontend/src/components/App.tsx
+++ frontend/mirror-frontend/src/components/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import TemperatureDisplay from './weather';
 import CalendarUI from './calendar';
+import Clock from './clock'; // Import the Clock component, assumes Clock.js is in the same directory
 import './App.css'; // Ensure this is the path to your CSS file
+import './clock.css'; // Ensure this is the path to your Clock CSS file
 
 const UI = () => {
   return (
@@ -12,8 +14,12 @@ const UI = () => {
       <div className="calendar-box">
         <CalendarUI />
       </div>
+      <div className="clock-box">
+        <Clock title="Local Time" />
+      </div>
     </div>
   );
 };
 
 export default UI;
+
