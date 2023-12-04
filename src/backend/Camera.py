2208ba7fa74bf592e8ecd0fd70ee402b0bbdbca9
All-Sait <lydecharles@yahoo.com>
2023-12-04 01:43:42 -0500

    Camera detection now properly affects display

--- src/backend/Camera.py
+++ src/backend/Camera.py
@@ -2,6 +2,7 @@ import cv2
 import os;
 import time;
 import ctypes;
+import subprocess;
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -10,18 +11,19 @@ camera = cv2.VideoCapture(0)
 print("Starting")
 os.popen("xset -display :0.0 dpms force off")
 interval = 0
+lastsignal = 0
+DISPLAY_TIMEOUT = 10
 while True:
+    current = time.time()
     ret, frame = camera.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4, minSize=(30, 30))
-    if len(faces) > 0:
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(20, 20))
+    override = current-lastsignal<5
+    if len(faces) > 0 (current-lastsignal<5):
        print("Face detected")
-       os.popen("xset -display :0.0 dpms force on")
-       interval = 60
-    else:
+       subprocess.call('xset dpms force on',shell=True)
+       if not override:
+          interval = 60
+    elif current-lastsignal>DISPLAY_TIMEOUT:
         os.popen("xset -display :0.0 dpms force off")
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
     time.sleep(interval)
-camera.release()
-cv2.destroyAllWindows()
