cc7e824c15674a64d31cdc3df94fdd795589fb49
Saitama <tuf80075@temple.edu>
2023-12-04 16:06:29 -0500

    Update module_view.tsx

--- frontend/progressive-app/src/app/dashboard/module_view.tsx
+++ frontend/progressive-app/src/app/dashboard/module_view.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
-import './ToggleSwitch.css';
+import './toggleswitch.css';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 if (!supabaseUrl || !supabaseAnonKey) {
