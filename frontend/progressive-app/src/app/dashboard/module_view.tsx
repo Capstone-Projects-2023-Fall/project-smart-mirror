8a277491d422b19b37c632e5570c5286c7b7cd38
Tony <tuk92144@temple.edu>
2023-12-04 13:43:11 -0500

    Update module_view.tsx

--- frontend/progressive-app/src/app/dashboard/module_view.tsx
+++ frontend/progressive-app/src/app/dashboard/module_view.tsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
 import './ToggleSwitch.css';
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and Anon Key must be defined');
+}
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 const ModuleView: React.FC = () => {
   const [widgetVisibility, setWidgetVisibility] = useState({
     Weather: true,
