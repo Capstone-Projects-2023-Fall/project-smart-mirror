51bb9436ed7784c2ba89809bb1dc93989caa1ff3
tonytran40 <tonytran40@gmail.com>
2023-12-03 23:01:44 -0500

    Update App.tsx

--- frontend/mirror-frontend/src/components/App.tsx
+++ frontend/mirror-frontend/src/components/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TemperatureDisplay from './weather';
 import CalendarUI from './calendar';
-import Clock from './clock'; // Import the Clock component, assumes Clock.js is in the same directory
-import './App.css'; // Ensure this is the path to your CSS file
-import './clock.css'; // Ensure this is the path to your Clock CSS file
+import FitbitDataComponent from './fitbit';
+import NewsComponent from './news';
+import './App.css';
 
-const UI = () => {
+const UI: React.FC = () => {
+  const [widgetVisibility, setWidgetVisibility] = useState({
+    Weather: true,
+    Calendar: true,
+    News: true,
+    Fitbit: true
+  });
+
+  useEffect(() => {
+    const fetchWidgetVisibility = async () => {
+      try {
+        const response = await fetch('http://localhost:1023/api/widget-visibility'); 
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setWidgetVisibility(prevVisibility => ({
+          ...prevVisibility,
+          ...data
+        }));
+      } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+      }
+    };
+
+    fetchWidgetVisibility();
+    const intervalId = setInterval(fetchWidgetVisibility, 5000); // Poll every 5 seconds
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className="ui-container">
-      <div className="weather-box">
-        <TemperatureDisplay />
+    <div className="ui-container2">
+      <div className="ui-container">
+        {widgetVisibility.Weather && (
+          <div className="weather-box">
+            <TemperatureDisplay />
+          </div>
+        )}
+        {widgetVisibility.Calendar && (
+          <div className="calendar-box">
+            <CalendarUI />
+          </div>
+        )}
       </div>
-      <div className="calendar-box">
-        <CalendarUI />
-      </div>
-      <div className="clock-box">
-        <Clock title="Local Time" />
-      </div>
+      {widgetVisibility.News && (
+        <div className="news-box">
+          <NewsComponent />
+        </div>
+      )}
+      {/* {widgetVisibility.Fitbit && (
+        <div className="fitbit-box">
+          <FitbitDataComponent />
+        </div>
+      )} */}
     </div>
   );
 };
 
 export default UI;
-
