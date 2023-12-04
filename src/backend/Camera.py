d352e6a703c1bdf7032b45de0043e59610409529
Saitama <tuf80075@temple.edu>
2023-12-04 02:30:22 -0500

    Update Camera.py

--- src/backend/Camera.py
+++ src/backend/Camera.py
@@ -19,11 +19,11 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(20, 20))
     override = current-lastsignal<5
-    if len(faces) > 0 or (current-lastsignal<5):
+    if len(faces) > 0 or (current-lastsignal<10):
        print("Face detected")
        subprocess.call('xset dpms force on',shell=True)
-       if not override:
-          interval = 60
+      # if not override:
+          #interval = 60
     elif current-lastsignal>DISPLAY_TIMEOUT:
         os.popen("xset -display :0.0 dpms force off")
     time.sleep(interval)
