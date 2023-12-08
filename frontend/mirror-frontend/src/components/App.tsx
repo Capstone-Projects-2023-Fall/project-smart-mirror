ec018d580d62a2bb24fd3941c33b4b59c66608f9
All-Sait <lydecharles@yahoo.com>
2023-12-08 17:05:42 -0500

    Widgets convereted to draggable

--- frontend/mirror-frontend/src/components/App.tsx
+++ frontend/mirror-frontend/src/components/App.tsx
@@ -8,6 +8,7 @@ import Clock from "./clock";
 import GlobalStyle from './GlobalStyle'; // Import the GlobalStyle
 import './App.css';
 import './clock.css';
+import { motion, useAnimation } from 'framer-motion';
 
 
 const UI: React.FC = () => {
@@ -43,32 +44,77 @@ const UI: React.FC = () => {
     return () => clearInterval(intervalId);
   }, []);
 
+  const upperCornerDeadzone = {
+    top: 0,
+    left: 0,
+    right: window.innerWidth * 0.2, // Adjust the width of the deadzone as needed
+    bottom: window.innerHeight * 0.2 // Adjust the height of the deadzone as needed
+  };
+
+
+  const initialPositionsString = localStorage.getItem('widgetPositions');
+  const initialPositions =  {
+    weatherBox: { x: 1370, y: 15 },
+    marketBox: { x: 800, y: 15 },
+    newsBox:{x: 15, y: 660 },
+  };
+  const [positions, setPositions] = useState(initialPositions);
+
+  const handleDragEnd = (event: MouseEvent | TouchEvent | PointerEvent, info: { point: { x: number, y: number } }, key: string) => {
+    // Save the new position to state and local storage
+    const updatedPositions = { ...positions, [key]: { x: info.point.x, y: info.point.y } };
+    setPositions(updatedPositions);
+    localStorage.setItem('widgetPositions', JSON.stringify(updatedPositions));
+  };
+
   return (
     <>
-    <GlobalStyle />
-      <div className="clock-box">
+      <GlobalStyle />
+      <motion.div className="clock-box">
         <Clock />
-      </div>
-      {widgetVisibility.News && (
-        <div className="news-box">
-          <NewsComponent />
-        </div>
-      )}
-      <div className="ui-container">
-        {widgetVisibility.Weather && (
-          <div className="weather-box">
-            <TemperatureDisplay />
-          </div>
-        )}
-        <MarketDisplay />
-
-      </div>
+      </motion.div>
       
-      {widgetVisibility.Fitbit && (
-        <div className="fitbit-box">
-          <FitbitDataComponent />
-        </div>
-      )}
+      <motion.div className="ui-container"> 
+
+        {widgetVisibility.News && (
+          <motion.div
+            className="news-box"
+            drag
+            initial={{ x: positions.newsBox.x, y: positions.newsBox.y }}
+            dragConstraints={{ left: 0, right: window.innerWidth - 150, top: 0, bottom: window.innerHeight-55 }}
+            onDragEnd={(event, info) => handleDragEnd(event, info, 'newsBox')}
+            dragElastic={0}
+          >
+            <NewsComponent />
+          </motion.div>
+        )}
+
+        {widgetVisibility.Weather && (
+          <motion.div 
+            className="weather-box"
+            drag 
+            initial={{ x: positions.weatherBox.x, y: positions.weatherBox.y }}
+
+            dragConstraints={{ left: 0, right: window.innerWidth - 150, top: 0, bottom: window.innerHeight-55 }}
+            onDragEnd={(event, info) => handleDragEnd(event, info, 'weatherBox')}
+
+            dragElastic={0}
+          >
+          <TemperatureDisplay/>
+        </motion.div>
+        )}
+         <motion.div
+            className="market-box"
+            drag
+            initial={{ x: positions.marketBox.x, y: positions.marketBox.y }}
+
+            dragConstraints={{ left: 0, right: window.innerWidth, top: 0, bottom: window.innerHeight }}
+
+          >
+          <MarketDisplay />
+        </motion.div>
+      </motion.div>
+     
     </>
   );
 };
