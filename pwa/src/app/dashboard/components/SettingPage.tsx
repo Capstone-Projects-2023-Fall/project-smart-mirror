0be1b683087885ee08d9c2ca58b127baf5248177
tonytran40 <tonytran40@gmail.com>
2023-12-09 15:40:51 -0500

    To be able to swiitch location off an on

--- pwa/src/app/dashboard/components/SettingPage.tsx
+++ pwa/src/app/dashboard/components/SettingPage.tsx
@@ -1,14 +1,63 @@
 import { useState } from 'react';
 import { Switch } from '@radix-ui/react-switch';
+import { supabase } from "@/app/utils/supabase-client";
 
 export default function SettingsPage() {
   const [locationEnabled, setLocationEnabled] = useState(false);
   const [trackingEnabled, setTrackingEnabled] = useState(false);
   const [analyticsEnabled, setAnalyticsEnabled] = useState(false);
+/*For toggle check*/
+  const LocationToggle = (checked) => setLocationEnabled(checked);
+  const TrackingToggle = (checked) => setTrackingEnabled(checked);
+  const AnalyticsToggle = (checked) => setAnalyticsEnabled(checked);
+/*For location*/
+const [toggleLocationSharing, setToggleLocationSharing] = useState(false)
+const handleLocationToggle = async (checked) => {
+  setLocationEnabled(checked); // Update the switch's state
+  const { data: { user }, error: userError } = await supabase.auth.getUser();
+  
+  // Check if the user data and ID are defined
+  if (userError || !user || !user.id) {
+    console.error(userError || 'User not found or user ID is undefined.');
+    setToggleLocationSharing(false);
+    return;
+  }
 
-  const handleLocationToggle = (checked) => setLocationEnabled(checked);
-  const handleTrackingToggle = (checked) => setTrackingEnabled(checked);
-  const handleAnalyticsToggle = (checked) => setAnalyticsEnabled(checked);
+  // If the toggle is turned on, get and update the location
+  if (checked) {
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+      const loc = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude,
+      };
+      
+      const { error } = await supabase
+        .from("profiles")
+        .update({ location: loc })
+        .eq("id", user.id);
+
+      if (error) {
+        console.error(error);
+        setToggleLocationSharing(false); // Revert the switch to off if there's an error
+      }
+    }, (error) => {
+      console.error(error.message);
+      setToggleLocationSharing(false); // Revert the switch to off if geolocation fails
+    }, {
+      enableHighAccuracy: true,
+    });
+  } else {
+    // If the toggle is turned off, remove the location
+    const { error } = await supabase
+      .from("profiles")
+      .update({ location: null }) // Set location to null or an empty object
+      .eq("id", user.id);
+
+    if (error) {
+      console.error(error);
+    }
+  }
+};
 
   return (
     <div className="text-skin-base">
@@ -31,7 +80,7 @@ export default function SettingsPage() {
               Tracking
               <Switch
                 checked={trackingEnabled}
-                onCheckedChange={handleTrackingToggle}
+                onCheckedChange={TrackingToggle}
                 className="bg-skin-button-muted relative inline-flex items-center h-6 rounded-full w-11"
               >
                 <span className={`block w-4 h-4 bg-skin-button-accent rounded-full shadow-lg transform transition-transform duration-200 ${trackingEnabled ? 'translate-x-6' : 'translate-x-1'}`}></span>
@@ -41,7 +90,7 @@ export default function SettingsPage() {
               Analytics
               <Switch
                 checked={analyticsEnabled}
-                onCheckedChange={handleAnalyticsToggle}
+                onCheckedChange={AnalyticsToggle}
                 className="bg-skin-button-muted relative inline-flex items-center h-6 rounded-full w-11"
               >
                 <span className={`block w-4 h-4 bg-skin-button-accent rounded-full shadow-lg transform transition-transform duration-200 ${analyticsEnabled ? 'translate-x-6' : 'translate-x-1'}`}></span>
@@ -55,23 +104,23 @@ export default function SettingsPage() {
           <ul className="list-disc pl-5">
             <li className="flex items-center justify-between mb-1">
               Change username
-              {/* Here you would handle the username changing logic */}
+              {/*log */}
             </li>
             <li className="flex items-center justify-between mb-1">
               Change password
-              {/* Here you would handle the password changing logic */}
+              {/* log */}
             </li>
             <li className="flex items-center justify-between mb-1">
               Change email
-              {/* Here you would handle the email changing logic */}
+              {/*log */}
             </li>
             <li className="flex items-center justify-between">
               Change birthday
-              {/* Here you would handle the birthday changing logic */}
+              {/* log */}
             </li>
             <li className="flex items-center justify-between">
               Change gender
-              {/* Here you would handle the gender changing logic */}
+              {/* log */}
             </li>
           </ul>
         </li>
