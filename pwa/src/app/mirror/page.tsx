114dacb821b4c17140c37a844640b9e214c189f7
devinrankin <devinrankin@temple.edu>
2023-12-11 07:21:02 -0500

    changing local ip route to website

--- pwa/src/app/mirror/page.tsx
+++ pwa/src/app/mirror/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Clock from "./clock"; // Time display
-import { motion, useAnimation } from 'framer-motion';
-import './App.css'
-import './clock.css'
-import './spotify.css';
-import Spotify from './spotify';
-import TemperatureDisplay from './weather';
-import NewsDisplay from './news'
-import MarketDisplay from './stocks';
-import FitbitDataComponent from './fitbit';
-import { QRCodeSVG } from 'qrcode.react';
+import { motion, useAnimation } from "framer-motion";
+import "./App.css";
+import "./clock.css";
+import "./spotify.css";
+import Spotify from "./spotify";
+import TemperatureDisplay from "./weather";
+import NewsDisplay from "./news";
+import MarketDisplay from "./stocks";
+import FitbitDataComponent from "./fitbit";
+import { QRCodeSVG } from "qrcode.react";
 import { supabase } from "../utils/supabase-client";
 import { useRouter } from "next/navigation";
 import { GoTrueClient } from "@supabase/supabase-js";
@@ -19,52 +19,52 @@ let refresh = false;
 
 type Props = {};
 
-
-
 export default function Mirror({}: Props) {
   const [widgetVisibility, setWidgetVisibility] = useState({
     Weather: true,
     Calendar: true,
     News: true,
     Fitbit: true,
-    Spotify: true
+    Spotify: true,
   });
 
-  const [userId,setUserId] = useState<string | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);
   const router = useRouter();
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const { data: users, error } = await supabase
-          .from('profiles') // Replace with your actual user table name
-          .select('*');
+          .from("profiles") // Replace with your actual user table name
+          .select("*");
 
         if (error) {
-          console.error('Error fetching users:', error.message);
+          console.error("Error fetching users:", error.message);
           return;
         }
 
         // Find the user with the matching mirrorID
-        const userWithMirrorID = users.find(user => user.mirrorID === '67ce79e9-b846-464c-bb44-1be8e0c9509a');
+        const userWithMirrorID = users.find(
+          (user) => user.mirrorID === "67ce79e9-b846-464c-bb44-1be8e0c9509a"
+        );
 
         if (userWithMirrorID) {
-          console.log('User with matching mirrorID:', userWithMirrorID);
-          if(refresh== false){
+          console.log("User with matching mirrorID:", userWithMirrorID);
+          if (refresh == false) {
             refresh = true;
-            router.refresh()
-            // refreshj here 
+            router.refresh();
+            // refreshj here
           }
-          setUserId(userWithMirrorID)
+          setUserId(userWithMirrorID);
         } else {
-          console.log('No user found with the specified mirrorID');
+          console.log("No user found with the specified mirrorID");
         }
-      } catch (error:any) {
-        console.error('Error fetching users:', error.message);
+      } catch (error: any) {
+        console.error("Error fetching users:", error.message);
       }
     };
     const intervalId = setInterval(fetchUserData, 15000);
-    if(!firstRun){
-      firstRun = true; 
+    if (!firstRun) {
+      firstRun = true;
       fetchUserData();
     }
     return () => clearInterval(intervalId);
@@ -72,7 +72,7 @@ export default function Mirror({}: Props) {
 
   useEffect(() => {
     const fetchWidgetVisibility = async () => {
-     /* try {
+      /* try {
         const response = await fetch('http://localhost:3000/api/widget?user_id=21380693-3ade-4951-82c0-1440aaf54297');
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -93,109 +93,127 @@ export default function Mirror({}: Props) {
     return () => clearInterval(intervalId);
   }, []);
 
-  const initialPositions =  { // pull from pwa
+  const initialPositions = {
+    // pull from pwa
     weatherBox: { x: 1765, y: 30 },
     marketBox: { x: 800, y: 15 },
-    newsBox:{x: 15, y: 880 },
-    spotifyBox:{x: 1570,y: 950},
-    fitbitBox:{x:15,y:430}
+    newsBox: { x: 15, y: 880 },
+    spotifyBox: { x: 1570, y: 950 },
+    fitbitBox: { x: 15, y: 430 },
   };
 
   const [positions, setPositions] = useState(initialPositions);
 
-  const handleDragEnd = (event: MouseEvent | TouchEvent | PointerEvent, info: { point: { x: number, y: number } }, key: string) => {
-    const updatedPositions = { ...positions, [key]: { x: info.point.x, y: info.point.y } };
+  const handleDragEnd = (
+    event: MouseEvent | TouchEvent | PointerEvent,
+    info: { point: { x: number; y: number } },
+    key: string
+  ) => {
+    const updatedPositions = {
+      ...positions,
+      [key]: { x: info.point.x, y: info.point.y },
+    };
     setPositions(updatedPositions);
-    localStorage.setItem('widgetPositions', JSON.stringify(updatedPositions));
+    localStorage.setItem("widgetPositions", JSON.stringify(updatedPositions));
   };
 
   const loggedin = userId != null;
-  if(loggedin){
+  if (loggedin) {
     return (
-    <>
-    {/* Assuming clock-box is styled to be full screen and centered */}
-      <motion.div className="fixed inset-0 flex justify-center items-center">
-        <Clock />
-      </motion.div>
-      <motion.div className="fixed inset-0"> 
-        {widgetVisibility.Weather && (
-            <motion.div 
+      <>
+        {/* Assuming clock-box is styled to be full screen and centered */}
+        <motion.div className="fixed inset-0 flex justify-center items-center">
+          <Clock />
+        </motion.div>
+        <motion.div className="fixed inset-0">
+          {widgetVisibility.Weather && (
+            <motion.div
               className="weather-box"
-              drag 
+              drag
               initial={{ x: positions.weatherBox.x, y: positions.weatherBox.y }}
               dragConstraints={{
                 left: 0,
                 right: window.innerWidth - 155,
                 top: 20,
-                bottom: window.innerHeight - 75
+                bottom: window.innerHeight - 75,
               }}
-              onDragEnd={(event, info) => handleDragEnd(event, info, 'weatherBox')}
+              onDragEnd={(event, info) =>
+                handleDragEnd(event, info, "weatherBox")
+              }
               dragElastic={0}
             >
               <TemperatureDisplay />
             </motion.div>
           )}
           {widgetVisibility.News && (
-            <motion.div 
+            <motion.div
               className="news-box"
-              drag 
+              drag
               initial={{ x: positions.newsBox.x, y: positions.newsBox.y }}
               dragConstraints={{
                 left: 15,
                 right: window.innerWidth - 100,
                 top: 0,
-                bottom: window.innerHeight - 185
+                bottom: window.innerHeight - 185,
               }}
-              onDragEnd={(event, info) => handleDragEnd(event, info, 'newsBox')}
+              onDragEnd={(event, info) => handleDragEnd(event, info, "newsBox")}
               dragElastic={0}
             >
               <NewsDisplay />
             </motion.div>
           )}
-          
+
           {widgetVisibility.Spotify && (
-            <motion.div 
+            <motion.div
               className="spotify-box"
-              drag 
+              drag
               initial={{ x: positions.spotifyBox.x, y: positions.spotifyBox.y }}
               dragConstraints={{
                 left: 0,
                 right: window.innerWidth - 350,
                 top: 65,
-                bottom: window.innerHeight - 120
+                bottom: window.innerHeight - 120,
               }}
-              onDragEnd={(event, info) => handleDragEnd(event, info, 'spotifyBox')}
+              onDragEnd={(event, info) =>
+                handleDragEnd(event, info, "spotifyBox")
+              }
               dragElastic={0}
             >
-            <Spotify id={userId} />
+              <Spotify id={userId} />
             </motion.div>
           )}
           {widgetVisibility.Fitbit && (
-            <motion.div 
+            <motion.div
               className="fitbit-box"
-              drag 
+              drag
               initial={{ x: positions.fitbitBox.x, y: positions.fitbitBox.y }}
               dragConstraints={{
                 left: 15,
                 right: window.innerWidth - 295,
                 top: 0,
-                bottom: window.innerHeight - 175
+                bottom: window.innerHeight - 175,
               }}
-              onDragEnd={(event, info) => handleDragEnd(event, info, 'fitbitBox')}
+              onDragEnd={(event, info) =>
+                handleDragEnd(event, info, "fitbitBox")
+              }
               dragElastic={0}
             >
-            <FitbitDataComponent id={userId} />
+              <FitbitDataComponent id={userId} />
             </motion.div>
           )}
-      </motion.div>
-    </>)
-    ;
-  
-  }else { 
+        </motion.div>
+      </>
+    );
+  } else {
     return (
       <div className="ui-container">
         <div className="QRCode">
-          <QRCodeSVG value={"http://10.0.0.225:3000/login?mirrorID=67ce79e9-b846-464c-bb44-1be8e0c9509a"} size={256} />
+          <QRCodeSVG
+            value={
+              "http://projectlumina.app/login?mirrorID=67ce79e9-b846-464c-bb44-1be8e0c9509a"
+            }
+            size={256}
+          />
         </div>
         <div className="qr-code-text">
           Please scan QR Code on Project Lumina App
