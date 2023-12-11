1516ffcdb1619749f354efaf4f651f814309b874
All-Sait <lydecharles@yahoo.com>
2023-12-11 14:32:31 -0500

    Fixes

--- pwa/src/app/mirror/spotify.tsx
+++ pwa/src/app/mirror/spotify.tsx
@@ -2,98 +2,112 @@ import React, { useState, useEffect } from 'react';
 import './spotify.css';
 import SpotifySVG from "./svg/spotify.svg";
 import Image from 'next/image';
+import { supabase } from "@/app/utils/supabase-client";
 
-type Props = {
-    id: string | null;
-};
+export default function Spotify() {
+    const [currentTrack, setCurrentTrack] = useState<any>(null);
 
-export default function Spotify( { id } : Props) {
-  const [currentTrack, setCurrentTrack] = useState<any>(null);
-  // Ensure this access token is valid and replace it with a new one if it has expired.
-  const accessToken = 'eyJhbGciOiJIUzI1NiIsImtpZCI6IjgveGRoY0ZEY3hVME56VUwiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzAyMjU1ODAyLCJpYXQiOjE3MDIyNTIyMDIsImlzcyI6Imh0dHBzOi8vc2FxemdkaW5wc2J4amR6aGdmdWUuc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6ImE2ZTJjOTM0LTU0MWMtNDkwNS04NTQxLWIwNDk2NjJmODdmMiIsImVtYWlsIjoidG9ueXRyYW40MEBnbWFpbC5jb20iLCJwaG9uZSI6IiIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIiwicHJvdmlkZXJzIjpbImVtYWlsIl19LCJ1c2VyX21ldGFkYXRhIjp7InVzZXJuYW1lIjoiS2luZCBFbGVwaGFudCJ9LCJyb2xlIjoiYXV0aGVudGljYXRlZCIsImFhbCI6ImFhbDEiLCJhbXIiOlt7Im1ldGhvZCI6InBhc3N3b3JkIiwidGltZXN0YW1wIjoxNzAyMjUyMjAyfV0sInNlc3Npb25faWQiOiI5MzUxYThiYS1hZTg3LTQ1ZGUtOTFmMy0yN2Y2YTUyNjBiZWIifQ.QOegshE2fqXhGgTZ1s0XrD0SC27PE3Y-GuvwDFewczA';
+    // Function to fetch the access token from Supabase
+    const fetchAccessToken = async () => {
+        // Call supabase.auth.getUser() to get the user details
+        const { data: authData, error: authError } = await supabase.auth.getUser();
 
-  useEffect(() => {
-    const fetchCurrentTrack = async () => {
+        // Check if there is an authenticated user
+        if (authError) {
+            console.error('Error fetching user:', authError);
+            return null;
+        }
+
+        if (authData.user) {
+            const { data, error } = await supabase
+                .from('profiles')
+                .select('spotify_access_token')
+                .eq('id', authData.user.id)
+                .single();
+
+            if (error) {
+                console.error('Error fetching access token:', error);
+                return null;
+            }
+
+            // Return the Spotify access token
+            return data.spotify_access_token;
+        } else {
+            // No user is logged in
+            console.error('No user logged in');
+            return null;
+        }
+    };
+
+    const fetchCurrentTrack = async (accessToken: string) => {
         const settings = {
             headers: {'Authorization': 'Bearer ' + accessToken}
-         }
-        const response = await fetch('https://api.spotify.com/v1/me', settings);
-        const data = await response.json()
+        };
+        
         try {
-          const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            },
-          });
+            const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', settings);
+            
+            if (response.status === 204) {
+                console.log('No track is currently playing.');
+                setCurrentTrack(null);
+                return;
+            }
 
-          // Check if the response has content before parsing it as JSON
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-      
-          // Spotify might return a 204 (No Content) if there is no track currently playing
-          if (response.status === 204) {
-            console.log('No track is currently playing.');
-            setCurrentTrack(null);
-            return;
-          }
-      
-          // Now we can safely parse the JSON
-          const data = await response.json();
-          console.log('API Response:', data);
-      
-          if (data && data.is_playing) {
-            setCurrentTrack(data.item);
-          } else {
-            setCurrentTrack(null);
-          }
+            const data = await response.json();
+            if (data && data.is_playing) {
+                setCurrentTrack(data.item);
+            } else {
+                setCurrentTrack(null);
+            }
         } catch (error) {
-          console.error('Error fetching the current track:', error);
-          setCurrentTrack(null);
+            console.error('Error fetching the current track:', error);
+            setCurrentTrack(null);
         }
-      };
-      
+    };
 
-    if (accessToken) {
-      fetchCurrentTrack();
-    }
-    const intervalId = setInterval(fetchCurrentTrack, 5000);
+    useEffect(() => {
+      let intervalId: NodeJS.Timeout;
 
-    // Clean up the interval on component unmount
-    return () => clearInterval(intervalId);
-  }, [accessToken]);
+        const init = async () => {
+            const token = await fetchAccessToken();
+            if (token) {
+                await fetchCurrentTrack(token); // Fetch once immediately
+                intervalId = setInterval(() => fetchCurrentTrack(token), 30000); // Then set up the interval
+            }
+        };
 
-  
+        init();
 
-  return (
-    <>
-       <div className="spotify-logobox">
-            {/* Use the imported SVG */}
-            <Image 
-                src={SpotifySVG.src} // Use the src from the WeatherSVG object
-                alt={'title'}
-                width={40} // Use the width from the WeatherSVG object
-                height={75} // Use the height from the WeatherSVG object
-            />  
-            {/* Now Playing text */}
-            <p className= "nowplaying">Now Playing</p>
-            {/* Additional weather information goes here */}
-        </div>
-        <div className="spotify-widget"> {} 
-            {currentTrack ? (
-            <div>
-                <h2 className="track-name">{currentTrack.name}</h2> {}
-                <p className="artist-name">{currentTrack.artists.map((artist: { name: any; }) => artist.name).join(', ')}</p> {}
-                {}
-                {currentTrack.album && currentTrack.album.images && currentTrack.album.images.length > 0 && (
-                <img className="album-cover" src={currentTrack.album.images[0].url} alt={`Album cover for ${currentTrack.name}`} />
+        // Cleanup function to clear the interval when the component unmounts
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []); // Empty dependency array indicates this effect runs once on mount
+
+    return (
+        <>
+            <div className="spotify-logobox">
+                <Image
+                    src={SpotifySVG.src}
+                    alt={'Spotify Logo'}
+                    width={40}
+                    height={75}
+                />
+                <p className="nowplaying">Now Playing</p>
+            </div>
+            <div className="spotify-widget">
+                {currentTrack ? (
+                    <div>
+                        <h2 className="track-name">{currentTrack.name}</h2>
+                        <p className="artist-name">{currentTrack.artists.map((artist: { name: any; }) => artist.name).join(', ')}</p>
+                        {currentTrack.album && currentTrack.album.images && currentTrack.album.images.length > 0 && (
+                            <img className="album-cover" src={currentTrack.album.images[0].url} alt={`Album cover for ${currentTrack.name}`} />
+                        )}
+                    </div>
+                ) : (
+                    <p>No track is currently playing.</p>
                 )}
             </div>
-            ) : (
-            <p>No track is currently playing.</p>
-            )}
-        </div>
-    </>
-  );
+        </>
+    );
 }
