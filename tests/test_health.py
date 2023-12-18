9cc04559541b97404b64d4dcd6f0b9431752abe0
MWilcox35 <123747098+MWilcox35@users.noreply.github.com>
2023-12-17 23:34:22 -0500

    Update test_health.py
    Commenting out old tests

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,12 +1,12 @@
-import pytest
-from src.backend.modules.health import calculate_bmi, displayBMI
-from src.backend.modules.health import get_steps
+#import pytest
+#from src.backend.modules.health import calculate_bmi, displayBMI
+#from src.backend.modules.health import get_steps
 
-def test_bmi():
-    assert calculate_bmi(70, 165) == 23.7
+#def test_bmi():
+#    assert calculate_bmi(70, 165) == 23.7
 
-def test_getsteps():
-    assert get_steps(1000) == 1000
-def test_Display_BMI():
-    expected_bmi = 23.7
-    assert displayBMI(70, 165) == expected_bmi
+#def test_getsteps():
+#    assert get_steps(1000) == 1000
+#def test_Display_BMI():
+#    expected_bmi = 23.7
+#    assert displayBMI(70, 165) == expected_bmi
